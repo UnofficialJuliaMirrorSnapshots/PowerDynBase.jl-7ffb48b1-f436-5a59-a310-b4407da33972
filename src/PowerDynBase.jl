dd03e4bff88f8766df6ab4aae277b8c2a02d96d2
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-30 04:45:43 +0000

    Snapshot of branch master taken on 2019-06-30T00:45:42.341-04:00 from "https://github.com/UnofficialJuliaMirror/PowerDynBase.jl-7ffb48b1-f436-5a59-a310-b4407da33972"

--- src/PowerDynBase.jl
+++ src/PowerDynBase.jl
@@ -3,88 +3,59 @@
 
 module PowerDynBase
 
-using Lazy: @>>
-using MacroTools
 using Markdown # for the @doc
-using Parameters: @with_kw
 
-#####################################################################
-# helper
-begin
-    approxzero(val) = isapprox(val, 0; atol=100*eps(Float64), rtol=0)
+include("common/Errors.jl")
+include("common/Helpers.jl")
+include("common/PowerGrid.jl")
+include("common/States.jl")
 
-    macro def(name, definition)
-        return quote
-            macro $(esc(name))()
-                esc($(Expr(:quote, definition)))
-            end
-        end
-    end
-
-    _removelinereferences(b) = b
-    function _removelinereferences(b::Expr)
-        if ~(b.head == :block)
-            return b
-        end
-        b.args = [ el for el in b.args if ~(el isa LineNumberNode) ]
-        return b
-    end
-    removelinereferences(q::Expr) = MacroTools.postwalk( _removelinereferences , q)
-    rlr = removelinereferences
-end
-
-#####################################################################
-
-
-
-# errors
-include("Errors.jl")
-
-# complex view (interpreting part of an array of real values as an array with complex values)
-include("complexview.jl")
-
-# DEVariables
-include("DEVariables.jl")
-
-# base definitions for all node dynamics
-include("NodeParametersBase.jl")
-include("NodeDynamicsBase.jl")
-
-# NodeSymbols
-include("NodeSymbols.jl")
+include("parsers/CSVParser.jl")
 
 # all possible node dynamics
-include("DynamicNodeMacro.jl")
-include("NodeDynamics/PQAlgebraic.jl")
-include("NodeDynamics/PVAlgebraic.jl")
-include("NodeDynamics/SlackAlgebraic.jl")
-include("NodeDynamics/SwingEquation.jl")
-include("NodeDynamics/FourthOrderEquation.jl")
-include("NodeDynamics/FourthOrderEquationGovernorExciterAVR.jl")
-include("NodeDynamics/VoltageSourceInverterMinimal.jl")
-include("NodeDynamics/VoltageSourceInverterVoltagePT1.jl")
-include("NodeDynamics/CurrentSourceInverterMinimal.jl")
-include("NodeDynamics/ExponentialRecovery.jl")
+include("nodes/NodeMacro.jl")
+include("nodes/PQAlgebraic.jl")
+include("nodes/PVAlgebraic.jl")
+include("nodes/SlackAlgebraic.jl")
+include("nodes/SwingEquation.jl")
+include("nodes/FourthOrderEq.jl")
+include("nodes/FourthOrderEqGovernorExciterAVR.jl")
+include("nodes/VoltageSourceInverterMinimal.jl")
+include("nodes/VoltageSourceInverterVoltagePT1.jl")
+include("nodes/CurrentSourceInverterMinimal.jl")
+include("nodes/ExponentialRecoveryLoad.jl")
 
+# all line types
+include("lines/LineMacro.jl")
+include("lines/StaticLine.jl")
+include("lines/PiModelLine.jl")
 
-# the structures building the grid dynamics from the node dynamics
-include("NetwirkRHSs.jl")
-include("GridDynamics.jl")
+include("operationpoint/operationpoint.jl")
 
-# States (kind of an interface)
-include("States.jl")
+include("simulations/PowerGridSolutions.jl")
+include("simulations/simulations.jl")
+
 
 # export of the main types and functions
 export GridDynamicsError, NodeDynamicsError, MissingParameterError, StateError
-export OrdinaryNodeDynamics, OrdinaryNodeDynamicsWithMass
-export no_internal_masses, no_internal_differentials
-export @DynamicNode, AbstractNodeParameters, showdefinition
-export construct_node_dynamics
-export NetworkRHS, GridDynamics
-export SystemSize, Nodes, AdmittanceLaplacian, masses, differentials
+export no_internal_masses
+export @DynamicNode, showdefinition
+export construct_vertex
+
 export State
-export internalsymbolsof, internaldsymbolsof, internaloutsymbolsof, parametersof
+
+export @Line, StaticLine
+export construct_edge
 
 export convert, promote_rule # only so the autodocs work properly
 
-end # module DPSABase
+export find_operationpoint
+
+export read_network_from_csv
+export PowerGrid
+export ode_function
+export systemsize
+export symbolsof
+export total_current
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,24 @@ using Test
 using Crayons
 
 testlist = [
-    ("nodedynamics.jl", "Single Node Tests"),
-    ("dynamicnodemacro.jl", "Dynamic Node Macro Tests"),
-    ("complexview.jl", "Complex View Tests"),
-    ("griddynamics.jl", "Grid Construction Tests"),
-    ("outputanderrors.jl", "Output and Error Tests"),
-    ("states.jl", "States Tests"),
+    ("nodes/PQAlgebraic.jl", "PQAlgebraic Node Tests"),
+    ("nodes/PVAlgebraic.jl", "PVAlgebraic Node Tests"),
+    ("nodes/SlackAlgebraic.jl", "SlackAlgebraic Node Tests"),
+    ("nodes/SwingEq.jl", "SwingEq Node Tests"),
+    ("nodes/SwingEqLVS.jl", "SwingEqLVS Node Tests"),
+    ("nodes/FourthOrderEq.jl", "FourthOrderEq Node Tests"),
+    ("nodes/VoltageSourceInverterMinimal.jl", "VoltageSourceInverterMinimal Node Tests"),
+    ("nodes/VoltageSourceInverterVoltagePT1.jl", "VoltageSourceInverterVoltagePT1 Node Tests"),
+    ("nodes/CurrentSourceInverterMinimal.jl", "CurrentSourceInverterMinimal Node Tests"),
+    ("nodes/ExponentialRecoveryLoad.jl", "ExponentialRecoveryLoad Node Tests"),
+    ("nodes/FourthOrderEqGovernorExciterAVR.jl", "FourthOrderEqGovernorExciterAVR Node Tests"),
+    ("lines/LineMacro.jl", "LineMacro tests"),
+    ("common/PowerGrid.jl", "PowerGrid Tests"),
+    ("common/States.jl", "States Tests"),
+    ("simulations/PowerGridSolutions.jl", "PowerGridSolutions Tests"),
+    ("simulations/simulations.jl", "Simulation Tests"),
+    ("operationpoint/operationpoint.jl", "operationpoint Tests"),
+    ("parsers/CSVParser.jl", "CSVParser Tests"),
 ]
 @testset "All Tests" begin
     @testset "$desc" for (file, desc) in testlist
